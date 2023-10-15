0498e205d6fd8cbbda9e31953acbd7a816ab4dd9
MalekBG <83009425+MalekBG@users.noreply.github.com>
2023-10-15 14:55:32 +0200

    challenge5

---
+++ MYOA MOOC/MalekBenGhorbel_chatterapp-challenge-5/js/channels.js
@@ -0,0 +1,30 @@
+const mockChannels = [
+    {
+        id: "channel1",
+        name: "The Boys",
+        favorite: true,
+        messages: [],
+        latestMessage: "14:50"
+    },
+    {
+        id: "channel2",
+        name: "MUC",
+        favorite: true,
+        messages: [],
+        latestMessage: "15:50"
+    },
+    {
+        id: "channel3",
+        name: "TUM",
+        favorite: false,
+        messages: [],
+        latestMessage: "16:50"
+    },
+    {
+        id: "channel4",
+        name: "Winfo",
+        favorite: false,
+        messages: [],
+        latestMessage: "17:50"
+    }
+]
